8ef126ef84cbe20e036bcf222a32dbbd8f5c5e1a
Harshal0902 <harshalraikwar07@gmail.com>
2025-11-11 00:01:54 +0530

    🐛 Fix deployment bug

--- next.config.js
+++ next.config.js
@@ -107,29 +107,29 @@ const config = {
             }
         ]
     },
-    // webpack: (config) => {
-    //     config.externals.push('pino-pretty', 'lokijs', 'encoding');
-    //     return config;
-    // },
-    webpack: (config, { isServer }) => {
-        config.resolve.alias = {
-            ...config.resolve.alias,
-            '@react-native-async-storage/async-storage': 'next/dist/compiled/react',
-            'react-native': 'react-native-web',
-        };
-
-        config.externals.push({
-            'pino-pretty': 'commonjs pino-pretty',
-            'pino': 'commonjs pino'
-        });
-        if (isServer) {
-            config.externals.push({
-                'bufferutil': 'bufferutil',
-                'utf-8-validate': 'utf-8-validate',
-            });
-        }
+    webpack: (config) => {
+        config.externals.push('pino-pretty', 'lokijs', 'encoding');
         return config;
     },
+    // webpack: (config, { isServer }) => {
+    //     config.resolve.alias = {
+    //         ...config.resolve.alias,
+    //         '@react-native-async-storage/async-storage': 'next/dist/compiled/react',
+    //         'react-native': 'react-native-web',
+    //     };
+
+    //     config.externals.push({
+    //         'pino-pretty': 'commonjs pino-pretty',
+    //         'pino': 'commonjs pino'
+    //     });
+    //     if (isServer) {
+    //         config.externals.push({
+    //             'bufferutil': 'bufferutil',
+    //             'utf-8-validate': 'utf-8-validate',
+    //         });
+    //     }
+    //     return config;
+    // },
     compiler: {
         removeConsole: process.env.NODE_ENV === 'production',
     }
